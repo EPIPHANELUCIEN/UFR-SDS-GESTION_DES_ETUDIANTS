20b5bdd2925ae0e73a7535712d315b1d1d8d11e7
Epiphane Niada <99919102+EPIPHANELUCIEN@users.noreply.github.com>
2022-05-12 22:00:17 +0200

    Add files via upload

---
+++ pages/accueil.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/bootstrap.min.css">
+    <title>Accueille</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <header class="col-12 ho">
+                <div>
+        <img src="../images/logo/kizerbo_logo-removebg-preview.png"" class="col-12 offset-6 logo">
+              </div>
+                <div>
+                   <p  class="col-12 offset-4 a">Universite <br></p> 
+                   <p class="col-12 offset-4 aa"> Joseph ki-Zerbo</p>
+                   
+                </div>
+                <div>
+                    <img src="../images/logo/4566a087e3b30da9024d888934a7eb36-removebg-preview.png" alt="" class="col-12 offset-11 acx">
+                </div>
+                <div class="horloge">
+                    <div class="heures col-11 offset-10 ab"></div>
+                    <div class="date col-12 offset-10 ac"></div>
+                 </div>  
+            </header>
+        </div>
+    </div>
+  
+    <div class="htext">
+BIENVENU SUR LE SITE DE UFR SDS DE L'UNIVERSITE JOSEPH KI-ZERBO
+    </div>
+    <hr class="hbr">
+    <div class="container-fluid">
+        <div class="row">
+
+            <section class=" hbr2">
+                <div class="go col-3">
+        <h3 class="mn">MENU</h3>
+       
+        <button class="button "><span id="btn">Ajouter un Administrateur </span></button>
+        <button class="button"><span id="btn">Ajouter un Tutteur </span></button>
+        <button class="button"><span id="btn">Ajouter un Etudiant </span></button>
+        <button class="button"><span id="btn">Consulter la Liste </span></button>
+        
+                </div>
+                <div class="col-7 crl go2">
+                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                          <div class="carousel-item active">
+                            <img src="../images/carrous/african-student-female-posed-with-backpack-and-school-items-on-yard-of-university-against-flags-of-different-countries.jpg" class="d-block w-100" alt="...">
+                          </div>
+                          <div class="carousel-item">
+                            <img src="../images/carrous/group-of-male-african-medical-students-in-college.jpg" class="d-block w-100" alt="...">
+                          </div>
+                          <div class="carousel-item">
+                            <img src="../images/carrous/young-african-students-wearing-facemasks-and-holding-books-and-phones-at-campus.jpg" class="d-block w-100" alt="...">
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
+                      <div class="go3">Des Etudiants disciplinés et travailleurs</div>
+                </div>
+            </section>
+
+        </div>
+    </div>
+    
+
+        <div class="container-fluid">
+            <div class="row">
+                <footer class="foot">
+                    <div>
+                        <img src="../images/logo/kizerbo_logo-removebg-preview.png"" class="col-12 offset-2 logo">
+                     </div>
+                     <div>
+                        <img src="../images/logo/4566a087e3b30da9024d888934a7eb36-removebg-preview.png" alt="" class="col-12 offset-10 acx">
+                    </div>
+                    <div class="col-2 offset-2 foott">
+                        <center>
+                        <h3>Nous contacter</h3>
+                       
+                        <ul>
+                            <li>(+226) 70 70 70 70</li>
+                            <li>(+226) 60 60 60 60</li>
+                            <li>(+226) 20 20 20 13</li>
+                            <li>universitékizerbo@gmail.com</li>
+                        </ul>
+                    </center>
+                    </div>
+                   
+                        <p class="col-2 offset-2 tetars">realiser par Epiphane niada</p>
+         
+                </footer>
+
+            </div>
+        </div>
+</body>
+<script src="../js/js.js"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+</html>
